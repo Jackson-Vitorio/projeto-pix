e89791bb2d9a330f233178b69d0bf2e32d9d9d9c
Jackson-Vitorio <136825439+Jackson-Vitorio@users.noreply.github.com>
2024-05-18 09:15:58 -0300

    criação base de carteiras:database

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,10 @@
 class Cliente < ApplicationRecord
+  after_create :create_carteira
+
+  private
+
+  def create_carteira
+    carteira = Carteira.create(saldo: 0)
+    self.update(carteira: carteira.id)
+  end
 end
